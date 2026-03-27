b5df21fb63dbfbd7570dbdd2f9b818a0f5ff51dd
Joginder <joginder.director@gmail.com>
2026-03-27 14:48:29 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'autoproapp',
+  webDir: 'www'
+};
+
+export default config;
