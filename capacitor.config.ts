99a7b962f7ce1571b694335eb573f446b13fcd97
Joginder <joginder.director@gmail.com>
2026-04-13 13:20:57 +0530

    fixed new issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,16 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'autoproapp',
   webDir: 'www',
+  overrideUserAgent: 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+  server: {
+    allowNavigation: ['https://api.americankeysupply.com', 'https://api.americankeysupply.com/V1/']
+  },
   plugins: {
     CapacitorHttp: {
       enabled: true,
     },
   },
+
 };
 
 export default config;
